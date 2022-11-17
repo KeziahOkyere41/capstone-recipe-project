d8ca1d0dc5d63eff2c3e14c1d41848416666e548
KeziahOkyere41 <nharnhayhaar208@gmail.com>
2022-11-16 19:09:17 -1200

    signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ before_action :authorize
 
 private
 
-defauthorize
-2current_user  User.find_by(id: session[:user_id])
+def authorize
+@current_user = User.find_by(id: session[:user_id])
 
 render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, :only: :create
+    skip_before_action :authorize, only: :create
 
     def create
         user = User.create!(user_params)
@@ -16,5 +16,5 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :password, :password_confirmation, :image_url, :bio)
     end
-    
+
 end
