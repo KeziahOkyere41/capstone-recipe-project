fecc5c5af62dcd5d0e1d6382ff666e2385f98dc1
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-11-18 11:29:49 +0000

    Configured cloudinary to the backend

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dz7qewa4e'
+  config.api_key = '482775351438583'
+  config.api_secret = 'ZWX_bORwHpWL3_3mlal7cVEfo-8'
+  config.secure = true
+end
