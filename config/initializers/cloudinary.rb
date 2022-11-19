d9969d6907d36aa6224c51ae91f3eefde1957f91
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-11-19 17:08:24 +0000

    Edited file

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'dz7qewa4e'
-  config.api_key = '482775351438583'
-  config.api_secret = 'ZWX_bORwHpWL3_3mlal7cVEfo-8'
+  config.cloud_name = ENV['CLOUD_NAME'],
+  config.api_key = ENV['CLOUD_API_KEY'],
+  config.api_secret = ENV['CLOUD_API_SECRET'],
   config.secure = true
 end
