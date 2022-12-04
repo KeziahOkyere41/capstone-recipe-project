96a557e5283ba24662a630f6ff6009864abd1981
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-12-04 13:01:16 +0000

    Styled recipe dtails

--- client/src/components/RecipeDetails.js
+++ client/src/components/RecipeDetails.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import CustomImage from './CustomImage';
+import './details.css';
 import { useParams } from 'react-router-dom';
 
 function RecipeDetails() {
-    const [recipe, setRecipe] = useState(null);
+    const [recipe, setRecipe] = useState([]);
     const params = useParams();
+    console.log(params)
     
     useEffect(() => {
         fetch(`/recipes/${params.id}`)
@@ -15,14 +16,30 @@ function RecipeDetails() {
     if (!recipe) return <h2>Loading...</h2>
     
     return (
-        <div className="recipe-card">
-            <CustomImage imgSrc={recipe.thumbnail} pt="65%"/>
-            <div className="recipe-card-info">
-                <img className="auther-img" src={recipe.thumbnail} alt=""/>
-                <p className="recipe-title">{recipe.title}</p>
-                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            </div>
-        </div>
+        <>
+                  <div className="content">
+                    <img src={recipe.thumbnail} alt="" />
+                    <div className="inner-content">
+                        <h1>{recipe.title}</h1>
+                        <h2>Can serve: {recipe.number_of_people_served} people</h2>
+                        <h3>Category: {recipe.categories}</h3>
+                        <h2>Can be prepared in: {recipe.duration}</h2>
+                    </div>
+                
+                    <div className="recipe-details">
+                        <div className="ingredients">
+                            <h2>Ingredients</h2><br />
+                            <h4>{recipe.ingredients}</h4>
+                        </div>
+                        <div className="instructions">
+                            <h2>Instructions</h2><br />
+                            <h4>{recipe.procedures}</h4>
+                        </div>
+                    </div>
+               
+	         </div>
+                
+        </>
     );
 }
 
